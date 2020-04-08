06e02d3624a37037753a9ae71fd58b192fda809d
taoqf <tao_qiufeng@126.com>
2020-04-08 10:39:54 +0800

    init

--- readme.md
+++ readme.md
@@ -1,1 +1,146 @@
 # WebView
+
+在原生页面嵌入浏览器
+
+## 时序
+
+### 初始化
+
+原生应用启动 -> 原生页面初始化 -> webview组件初始化 -> web页面初始化 -> [web组件发送ready事件](https://www.npmjs.com/package/@mmstudio/aw000024)
+
+### 原生应用调用web页面
+
+初始化 -> 原生应用中触发事件 -> 原生应用调用web组件 -> web组件中的响应执行并返回值给原生应用
+
+上面第二步提到的事件，可以是用户事件（比如用户点击了原生页面上某个按钮），也可以是系统事件（比如系统初始化）
+
+示例
+
+比如我们有一个页面，分为两部分,上面部分为原生部分实现，有一个展示文字和一个按钮
+
+```tsx
+<>
+	<Text>调用页面示例</Text>
+	<Button title='调用页面' onPress={a('a002')}></Button>
+	<WM000001 mm={mm} uri='http://server:port/pg001.html' onReady={a('a001')}></WM000001>
+</>
+```
+
+因为react的函数式组件的策略的限制，我们暂时还不能通过更方便的方法拿到webview组件对象，暂时我们可以这样操作:
+
+1. 在页面的tpl.tsx中引入函数
+
+	```ts
+	import React, { useRef } from 'react';
+	```
+
+1. 在页面的tpl.tsx中使用全局函数生成引用对象
+
+	```ts
+	const ref001 = useRef(null);
+	```
+
+1. 在webview组件添加属性ref
+
+	```tsx
+	<>
+		<WM000001 mm={mm} ref={ref001} uri='http://server:port/pg001.html' onReady={a('a001')}></WM000001>
+	</>
+	```
+
+1. 在按钮事件上附加上该参数
+
+	```tsx
+	<>
+		<Button title='调用页面' onPress={a('a002', ref001)}></Button>
+	</>
+	```
+
+	完整的tpl.tsx如下：
+
+	```tsx
+	import am1 from '@mmstudio/am000001';
+	import React, { useRef } from 'react';
+	import { Button, Text } from 'react-native';
+	import WM000001 from '@mmstudio/wm000001';
+
+	export default function tpl(a: <T>(action: string, ...args: unknown[]) => ((...args: unknown[]) => void), s: (...class_names: string[]) => {}, d: <T>(d: string) => T, mm: am1) {
+		const ref001 = useRef(null);
+		return (<>
+			<Text>调用页面示例</Text>
+			<Button title='调用页面' onPress={a('a002', ref001)}></Button>
+			<WM000001 mm={mm} ref={ref001} uri='http://server:port/pg001.html' onReady={a('a001')}></WM000001>
+		</>);
+	}
+	```
+
+1. 在响应a002.ts中调用webview控件的fire方法
+
+	```ts
+	import am1 from '@mmstudio/am000001';
+	import { RefObject } from 'react';
+	import WM000001 from '@mmstudio/wm000001';
+
+	export default async function a002(mm: am1, e: unknown, ref: RefObject<WM000001>, ...args: unknown[]) {
+		const webview = ref.current!;
+		interface Result {
+			// todo
+		}
+		const r001 = await webview.fire<Result>('a002', { foo: 'bar' }, 3000);
+		}
+	}
+	```
+
+### web页面调用原生应用
+
+初始化 -> web页面触发事件 -> [调用原生应用](https://www.npmjs.com/package/@mmstudio/aw000025) -> 原生应用响应执行并返回值 -> web页面拿到原生应用返回值
+
+相比较来说，web页面调用原生应用比较简单，首先，没有只能在一个web组件中调用的限制,其次，也没有必须发送ready事件的限制。第三，可以使用原子操作完成调用，更方便。
+
+示例
+
+比如我们有一个web页面，其中某个组件中有一个按钮，用户点击时调用响应a001
+
+```html
+<input type="button" value="call native" data-mm-actions='click:a001'>
+```
+
+那么我们在响应a001.ts中可以通过调用原子操作即可
+
+```ts
+import aw1 from '@mmstudio/aw000001';
+import aw25 from '@mmstudio/aw000025';
+
+export default async function a001(mm: aw1) {
+	// 调用原生响应
+	/**
+	 * 原生响应结果
+	 */
+	const r1586312934 = await (() => {
+		const p1 = 'a005';	// 原生事件/响应名
+		const p2 = { foo: 'mm', bar: 123 };	// 原生事件/响应参数
+		const p3 = 3000;	// 超时时间
+		interface Result {
+			mm: string;
+		}
+		return aw25<Result>(p1, p2, p3);
+	})();
+	alert(`原生响应返回结果${JSON.stringify(r1586312934)}`);
+}
+```
+
+## 其它
+
+在混合型app应用中，建议尽量将所有业务逻辑放置在web应用中，这样可以大大加速页面的开发过程。
+
+每个web页面中必须有且只能有一个web组件[发送ready事件](https://www.npmjs.com/package/@mmstudio/aw000024)后，才可以实现从原生应用到web页面的调用，否则，只能单向从web页面调用原生应用
+
+有动画及对时效性要求非常高的页面（如首页展示部分），需用原生实现。
+
+web页面跳转如果需要动画，创建多个原生页面，嵌入不同的web页面来实现，页面跳转不使用超链接，而是[调用原生响应](https://www.npmjs.com/package/@mmstudio/aw000025)，在原生响应中调用原子操作来实现页面跳转。
+
+视频播放部分使用原生实现，可达到更流畅的效果，且支持视频格式更丰富。
+
+调用系统功能使用原生实现，由web组件通过[原子操作](https://www.npmjs.com/package/@mmstudio/aw000025)调用
+
+webview组件和原生应用共享cookie，所以，建议将所有服务将放置在web应用中。如果web应用占的比重较少，也可以通过nginx同域部署，在同域下共享cookie的方式来实现。
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,401 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Animated, Easing, ImageSourcePropType, Platform, ProgressBarAndroid, ProgressViewIOS, StyleProp, View, ViewStyle } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { WebViewMessageEvent, WebViewProgressEvent } from 'react-native-webview/lib/WebViewTypes';
+import am1 from '@mmstudio/am000001';
 
-interface IProps {
+interface IOptionsState {
+	progress: number;
+	flag: boolean;
+	InnerImage: ImageSourcePropType;
+	OuterImage: ImageSourcePropType;
 }
 
-export default function Widget(props: IProps) {
-	return <>
-	</>;
+interface IBaseProps {
+	mm: am1;
+	/**
+	 * 进度条的环部分
+	 */
+	progressOuterImage?: string | number;
+	/**
+	 * 进度条的中间部分
+	 */
+	progressInnerImage?: string | number;
+	/**
+	 * 进度条的颜色(顶部)
+	 */
+	progressBarColor?: string;
+	/**
+	 * Boolean value that forces the `WebView` to show the loading view
+	 * on the first load.
+	 */
+	startInLoadingState?: boolean;
+	/**
+	 * Boolean value that determines whether a horizontal scroll indicator is
+	 * shown in the `WebView`. The default value is `true`.
+	 */
+	showsHorizontalScrollIndicator?: boolean;
+	/**
+	 * Boolean value that determines whether a vertical scroll indicator is
+	 * shown in the `WebView`. The default value is `true`.
+	 */
+	showsVerticalScrollIndicator?: boolean;
+	/**
+	 * Boolean that controls whether the web content is scaled to fit
+	 * the view and enables the user to change the scale. The default value
+	 * is `true`.
+	 *
+	 * On iOS, when `useWebKit=true`, this prop will not work.
+	 */
+	scalesPageToFit?: boolean;
+	/**
+	 * Boolean that determines whether HTML5 audio and video requires the user
+	 * to tap them before they start playing. The default value is `true`.
+	 */
+	mediaPlaybackRequiresUserAction?: boolean;
+	/**
+	 * List of origin strings to allow being navigated to. The strings allow
+	 * wildcards and get matched against *just* the origin (not the full URL).
+	 * If the user taps to navigate to a new page but the new page is not in
+	 * this whitelist, we will open the URL in Safari.
+	 * The default whitelisted origins are "http://*" and "https://*".
+	 */
+	originWhitelist?: string[];
+	/**
+	 * Should caching be enabled. Default is true.
+	 */
+	cacheEnabled?: boolean;
+	/**
+	 * 加载时的背景颜色
+	 */
+	loadingBackgroundColor?: string;
+	style?: StyleProp<ViewStyle>;
+
+	onReady?(): void;
+	onError?(): void;
+	onLoad?(): void;
+	onLoadEnd?(): void;
+	onLoadStart?(): void;
+}
+
+interface IProps1 extends IBaseProps {
+	uri: string;
+}
+
+interface IProps2 extends IBaseProps {
+	html: string;
+}
+
+type Props = IProps1 | IProps2;
+
+function responseweb(webview: WebView, { id }: IMsg, result: unknown) {
+	// 这时，web中应已注册全局函数 function m${id}(content: unknown);等待调用
+	// 调用web中已注册的函数，web中收到该函数后（或超时）应反注册（删除）该全局函数,该函数如果不存在也不会抛出异常，所以这里可以不用处理函数不存在的情况
+	const content = JSON.stringify(result === undefined ? null : result);
+	webview.injectJavaScript(`m${id}(${JSON.stringify(content)});`);	// 多一层`JSON.stringify`是为了将其变为代码的时候保证是双引号括起来的, 如content为null时， m001("null")
+}
+
+export default class extends Component<Props, IOptionsState> {
+	public static defaultProps = {
+		loadingBackgroundColor: 'white',
+		progressBarColor: 'red'
+	};
+	private spinValue: Animated.Value;
+	private scaleValue: Animated.Value;
+	private opacityValue: Animated.Value;
+	private spinAnim: Animated.CompositeAnimation;
+	private scaleAnim: Animated.CompositeAnimation;
+	private webview: WebView;
+	public constructor(props: Props, context?: unknown) {
+		super(props, context);
+		this.state = {
+			...props,
+			InnerImage: (() => {
+				if (typeof this.props.progressInnerImage === 'string') {
+					return { uri: this.props.progressInnerImage };
+				} else if (typeof this.props.progressInnerImage === 'number') {
+					return this.props.progressInnerImage;
+				}
+				return require('./images/logo.png');
+			})(),
+			OuterImage: (() => {
+				if (typeof this.props.progressOuterImage === 'string') {
+					return { uri: this.props.progressOuterImage };
+				} else if (typeof this.props.progressOuterImage === 'number') {
+					return this.props.progressOuterImage;
+				}
+				return require('./images/circle.png');
+			})(),
+			flag: false,
+			progress: 0
+		};
+		this.spinValue = new Animated.Value(0);
+		this.scaleValue = new Animated.Value(0);
+		this.opacityValue = new Animated.Value(1);
+	}
+
+	public componentDidMount() {
+		this.spin();
+		this.scale();
+	}
+
+	private scale() {
+		this.opacityValue.setValue(1);
+		this.scaleAnim = Animated.sequence([
+			Animated.timing(this.scaleValue, { toValue: 1, useNativeDriver: true, duration: 200 }),
+			Animated.delay(500),
+			Animated.timing(this.opacityValue, { toValue: 0, useNativeDriver: true }),
+			Animated.timing(this.scaleValue, { toValue: 0, useNativeDriver: true, duration: 200 })
+		]);
+		this.scaleAnim.start(() => {
+			if (this.state.progress !== 1) {
+				this.scale();
+			}
+		});
+	}
+
+	private spin() {
+		this.spinValue.setValue(0);
+		this.spinAnim = Animated.timing(this.spinValue, {
+			duration: 800,
+			easing: Easing.linear,
+			toValue: 1,
+			useNativeDriver: true
+		});
+		this.spinAnim.start(() => {
+			if (this.state.progress !== 1) {
+				this.spin();
+			} else {
+				// 不加延时的话logo会跟着转
+				setTimeout(() => {
+					this.setState({
+						flag: true
+					});
+				}, 0);
+			}
+		});
+	}
+
+	public fire<T>(action: string, msg: unknown, timeout: number) {
+		const id = uuid();
+		const m = {
+			action,
+			content: msg === undefined ? null : msg,
+			id,
+			type: MessageType.native2web
+		};
+		const content = JSON.stringify(m);
+		const webview = this.webview;
+		// !!! ExceptionsManager.js:82 Error evaluating injectedJavaScript: This is possibly due to an unsupported return type. Try adding true to the end of your injectedJavaScript string.
+		// !!! we must add `true;` at the end of the script.
+		webview.injectJavaScript(`mmrn(${JSON.stringify(content)});true;`);
+		return add<T>(m, timeout);
+	}
+	public render() {
+		if (this.state.progress === 1) {
+			this.scaleAnim.stop();
+			this.spinAnim.stop();
+		}
+		return (<View
+			style={{
+				backgroundColor: '#fff',
+				flex: 1
+			}}
+		>
+			{this.render_progress_bar()}
+			{this.renderWebView()}
+			{this.renderLoading()}
+		</View >
+		);
+	}
+
+	private render_progress_bar() {
+		const props = this.props as IProps1;
+		if (props.uri && this.state.progress !== 1) {
+			if (Platform.OS === 'ios') {
+				return <ProgressViewIOS
+					progress={this.state.progress}
+					progressTintColor={this.props.progressBarColor}
+					progressViewStyle='bar'
+				></ProgressViewIOS>;
+			} else if (Platform.OS === 'android') {
+				return <ProgressBarAndroid
+					color={this.props.progressBarColor}
+					indeterminate={false}
+					progress={this.state.progress}
+					style={{ height: 4 }}
+					styleAttr='Horizontal'
+				></ProgressBarAndroid>;
+			}
+		}
+		return undefined;
+	}
+
+	private renderLoading() {
+		const props = this.props as IProps1;
+		if (props.uri && this.state.flag === false) {
+			const outersource = this.state.OuterImage;
+			const innersource = this.state.InnerImage;
+			const spin = this.spinValue.interpolate({
+				inputRange: [0, 1],
+				outputRange: ['0deg', '360deg']
+			});
+			return <View
+				style={{
+					alignItems: 'center',
+					backgroundColor: this.props.loadingBackgroundColor,
+					height: '100%',
+					justifyContent: 'center'
+				}}
+			>
+				<Animated.Image
+					source={outersource}
+					style={{
+						height: 50,
+						opacity: this.opacityValue,
+						transform: [
+							{
+								rotate: spin
+							},
+							{
+								scale: this.scaleValue
+							}
+						],
+						width: 50
+					}}
+				>
+				</Animated.Image>
+				<Animated.Image
+					source={innersource}
+					style={[
+						{
+							height: 40,
+							opacity: this.opacityValue,
+							position: 'absolute',
+							width: 40
+						},
+						{
+							transform: [
+								{
+									scale: this.scaleValue
+								}
+							]
+						}
+					]}
+				>
+				</Animated.Image>
+			</View>;
+		}
+		return undefined;
+	}
+
+	private renderWebView() {
+		const props = this.props as (IProps1 & IProps2);
+		const { mm, uri, html } = props;
+		const emit = mm.emit;
+		if (!uri && !html) {
+			throw new Error('Please set the URI or HTML properties of WebView');
+		}
+		const source = (() => {
+			if (uri) {
+				return { uri };
+			}
+			return { html };
+		})() as { uri: string } | { html: string };
+		return <WebView
+			{...props}
+			allowsBackForwardNavigationGestures={true}
+			onMessage={async (event: WebViewMessageEvent) => {
+				const data = JSON.parse(event.nativeEvent.data) as IMsg;
+				const webview = this.webview;
+				switch (data.type) {
+					case MessageType.webready:
+						{
+							const ready = props.onReady;
+							if (ready) {
+								ready();
+							}
+						}
+						break;
+					case MessageType.native2web:
+						back(data);
+						break;
+					case MessageType.web2native:
+						// 这时，web中应已注册全局函数 function m${id}(content: unknown);等待调用
+						{
+							const ret = await emit(mm, data.action, data.content);	// 在响应中第一个参数为mm，第二个参数即web页面发过来的数据
+							responseweb(webview, data, ret);
+						}
+						// 这里无法拿到页面实例，无法通过 页面实例.refs[ctl_name] 访问到webview实例
+						break;
+					default:
+						throw new Error('Unknown message type');
+				}
+			}}
+			ref={(ins) => {
+				this.webview = ins!;
+			}}
+			source={source}
+			userAgent='mm-hybrid'
+			onLoadProgress={(res: WebViewProgressEvent) => {
+				this.setState({
+					progress: res.nativeEvent.progress
+				});
+			}}
+		>
+		</WebView>;
+	}
+}
+
+
+enum MessageType {
+	webready,
+	web2native,
+	native2web
+}
+
+interface IMsg {
+	id: string;
+	type: MessageType;
+	action: string; // a001
+	content: unknown;
+}
+
+interface IDeferred<T> {
+	resolve(result: T): void;
+	reject(reson: Error): void;
+}
+
+const pool = new Map<string, IDeferred<unknown>>();
+
+function add<T>(msg: IMsg, timeout: number) {
+	const defferred = {} as IDeferred<T>;
+	const id = msg.id;
+	const handler = setTimeout(() => {
+		defferred.reject(new Error('Timeout'));
+	}, timeout);
+	pool.set(msg.id, defferred);
+	return new Promise<T>((res, rej) => {
+		defferred.resolve = (value: T | PromiseLike<T>) => {
+			clearTimeout(handler);
+			pool.delete(id);
+			res(value);
+		};
+		defferred.reject = (reason: Error) => {
+			clearTimeout(handler);
+			pool.delete(id);
+			rej(reason);
+		};
+	});
+}
+
+function back({ id, content }: IMsg) {
+	const origin = pool.get(id);
+	if (origin) {
+		origin.resolve(content);
+	}
+}
+
+function uuid() {
+	return Math.random().toString().substr(2);
 }
